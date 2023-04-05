3610f8b5398ddb149d669426654a53c7e3521067
RuthMaria <ruth-15rms@hotmail.com>
2023-04-05 15:05:42 -0300

    feat: create components

--- src/app/tasks/task-list-item/task-list-item.component.ts
+++ src/app/tasks/task-list-item/task-list-item.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Task } from '../shared/task';
 
 @Component({
   selector: 'app-task-list-item',
   templateUrl: './task-list-item.component.html',
-  styleUrls: ['./task-list-item.component.css']
+  styleUrls: ['./task-list-item.component.css'],
 })
-export class TaskListItemComponent {
+export class TaskListItemComponent implements OnInit {
+  /* input é um atributo do componente*/
+  @Input()
+  task!: Task;
 
+  ngOnInit(): void {}
 }
--- src/app/tasks/task-list/task-list.component.ts
+++ src/app/tasks/task-list/task-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../shared/task';
+import { TaskService } from '../shared/task.service';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
-  styleUrls: ['./task-list.component.css']
+  styleUrls: ['./task-list.component.css'],
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
+  tasks: Task[] = [];
 
+  constructor(private taskService: TaskService) {}
+
+  /* esse método é usado apenas na inicialização do component, se comporta igual ao useEffect */
+  ngOnInit() {
+    this.tasks = this.taskService.getAll();
+  }
 }
