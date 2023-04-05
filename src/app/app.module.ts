3466a5ef5e1874741b507078ad1a357c31e11866
RuthMaria <ruth-15rms@hotmail.com>
2023-04-04 21:26:03 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './tasks/task-list/task-list.component';
+import { TaskListItemComponent } from './tasks/task-list-item/task-list-item.component';
+import { TaskFormComponent } from './tasks/task-form/task-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TaskListComponent, TaskListItemComponent, TaskFormComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
